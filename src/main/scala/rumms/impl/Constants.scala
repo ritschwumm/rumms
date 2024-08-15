fdfd35520c4727c6cea78503930a99b266eba611
Herr Ritschwumm <ritschwumm@djini.de>
2024-08-16 00:33:10 +0200

    upgrade deps, adapt code

--- src/main/scala/rumms/impl/Constants.scala
+++ src/main/scala/rumms/impl/Constants.scala
@@ -6,10 +6,10 @@ import scutil.time.implicits.*
 object Constants {
 	val version			= 1
 	val encoding		= Charsets.utf_8
-	val continuationTTL	= 1.minutes
+	val continuationTTL	= 1.duration.minutes
 	val clientTTL		= continuationTTL	*! 2
 	val conversationTTL	= clientTTL			*! 3
-	val sendDelay		= 100.millis
+	val sendDelay		= 100.duration.millis
 
 	object paths {
 		val code	= "/code"
